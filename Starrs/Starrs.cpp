c201ca2e353982b87c7f9449d9e50ad39430d74e
Irish <Irish@DESKTOP-IMKLUAF>
2024-04-24 21:28:44 +0300

    Добавьте файлы проекта.

---
+++ Starrs/Starrs.cpp
@@ -0,0 +1,21 @@
+﻿
+//звездочками рисует треугольник высотой n
+
+#include <iostream>
+
+char stringOfStars(int n) {
+	if (n == 1) return '*';
+	std::cout << stringOfStars(n - 1) << std::endl;
+	while (n) {
+		std::cout << '*';
+		n--;
+	}
+}
+
+int main()
+{
+	std::cout << "Input the number of strings" << std::endl;
+	int n = 0;
+	std::cin >> n;
+	if (n > 0) std::cout << stringOfStars(n);
+}
